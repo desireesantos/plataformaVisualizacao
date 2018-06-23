a60a33b12e0458dd454f0925e5565e2835113024
Desiree Santos <desiree.dossantos@sonicdrivein.com>
2018-06-22 22:32:21 -0300

    Create table to display data

--- hello.py
+++ hello.py
@@ -4,22 +4,45 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-  return render_template('template.html', header_data=header_data(), body_data=body_data(), name="acidentes-2000 | acidentes-2016")
+  return render_template('template.html', header_data=header_data(), body_data=body_data(), names= pathFiles())
 
 def header_data():
-	header = []
-	files = ['./data/acidentes-2000.csv', './data/acidentes-2016.csv']
 
- 	for file in files:
- 			with open (file, 'rb') as file:
- 				reader = csv.reader(file, delimiter=';')
- 			
-	 			for row in reader:
-	 					for r in row:
-	 						header.append(r)
-	 					break			
-	 		
-	return header;
+	header=[]
+ 	for file in getFiles():
+ 		with open (file, 'rb') as file:
+	  		createHeader(readCSVfile(file), header)
+
+	return sortedHeader(header)
+
+
+def sortedHeader(columms):
+	return sorted(list(set(columms)), key=str.lower)
+
+
+def readCSVfile(file):
+	return csv.reader(file, delimiter=';')
+
+
+def getFiles():
+	return ['./data/acidentes-2000.csv', './data/acidentes-2016.csv']
+
+def pathFiles():
+	names = []
+	for file in getFiles():
+		names.append(fileName(file))
+	return names
+
+def fileName(file):
+	name = (file.split('./data/'))[1]
+	return name.split('.csv')[0]	
+
+def createHeader(first_line, header):
+	for line in first_line:
+	 		for l in line:
+	 				header.append(l)
+	 		break
+	return header 							
 
 def body_data():
 	with open ('./data/acidentes-2000.csv', 'rb') as file:
