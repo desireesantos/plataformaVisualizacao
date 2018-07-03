ba87d157f9437cbfd581dbab3b884b74ab4258b5
Desiree Santos <desiree.dossantos@sonicdrivein.com>
2018-07-02 23:22:23 -0300

    Hide/show columns

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,44 @@
 
-let table = document.getElementById("containerTable");
-let thead = table.getElementsByTagName("thead")[0];
+function hideShowCols(tabId) {
+   'use  strict';
+    var tab, i, j, th, func = {}, cell ,first=true, startCol=0;
 
-thead.onclick = function (e) {
-   e = e || window.event;
-   var th = e.target || e.srcElement;
-
-   alert(th.innerHTML);
+    th = (window.event.target) ? window.event.target : window.event.srcElement;
+    if (th.tagName !== 'TH') {
+        return;
+    }
+    tab = document.getElementById(tabId);
+    function show(cell) {
+        if (cell.hide) {
+            cell.innerHTML = cell.innerHTML.slice(5, -3);
+            cell.hide = !cell.hide;
+        }
+    }
+    function hide(cell) {
+        if (!cell.hide) {
+            cell.innerHTML = '&#8203;<!--' + cell.innerHTML + '-->';
+            cell.hide = !cell.hide;
+            cell.style.backgroundColor = "lightgray";
+        }
+    }
+    if (th.hide) {
+        func = show;
+    } else {
+        func = hide;
+    }
+    for (i = 0; i < tab.rows.length; i++) {
+        for (j = startCol; j < tab.rows[i].cells.length; j++) {
+            cell = tab.rows[i].cells[j];
+            if ((th.offsetLeft <= cell.offsetLeft && cell.offsetLeft <= th.offsetLeft + th.clientWidth)
+                    && (th.colSpan >= cell.colSpan)) {
+                func(cell);
+                if(first){
+                    first=false;
+                    startCol=j;
+                }
+            } else if (cell.offsetLeft > th.offsetLeft + th.clientWidth) {
+                break;
+            }
+        }
+    }
 }
